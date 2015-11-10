35857f6d36f76548859462429d7e01f74e9a185a
Denis Domrachev <domrachev.mail@gmail.com>
2015-11-10 21:10:45 +0300

    game states

--- app/actors/WebSocketActor.scala
+++ app/actors/WebSocketActor.scala
@@ -1,15 +1,16 @@
 package actors
 
-import actors.WebSocketActor.{Value, Coordinate, StateCoordinates}
-import akka.actor.{Props, ActorLogging, Actor, ActorRef}
+import actors.WebSocketActor.{Coordinate, StateCoordinates, Value}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.event.LoggingReceive
 import akka.util.Timeout
+import play.api.libs.json.{JsValue, Json}
 
 import scala.concurrent.duration._
-import play.api.libs.json.{JsValue, Json}
 
 class WebSocketActor(out: ActorRef) extends Actor with ActorLogging {
   import Messages._
+
   import concurrent.ExecutionContext.Implicits.global
   implicit val timeout : Timeout = 1.second
 
@@ -18,17 +19,75 @@ class WebSocketActor(out: ActorRef) extends Actor with ActorLogging {
   implicit val playersWrites = Json.writes[Players]
   implicit val valueWrites = Json.writes[Value]
 
-  var myUid : String = _
-  var snake : ActorRef = _
   Lobby() ! Started
 
-  def receive = LoggingReceive {
+  def receive = initial
+
+  def initial = LoggingReceive {
+    case x : JsValue => (x \ "t").as[String] match {
+      case "FindPlayers" =>
+        Lobby() ! GetList
+      case _ =>
+    }
+    case x : Players =>
+      val myUid = x.me
+      out ! Json.toJson(x)
+      context.become(waitingForCommand(myUid))
+  }
+
+  def waitingForCommand(myUid: String) : Actor.Receive = LoggingReceive {
     case x : JsValue => (x \ "t").as[String] match {
       case "CreateGame" =>
-        val player1 = (x \ "player1").as[String]
         val player2 = (x \ "player2").as[String]
-        AllGames() ! Pair(player1, player2)
+        AllGames() ! Pair(myUid, player2)
         Lobby() ! GetCompany(player2)
+        context.become(sendingInvitationWaitingRoom(myUid))
+      case "FindPlayers" =>
+        Lobby() ! GetList
+      case "Connect" =>
+        val player = (x \ "player").as[String]
+        AllGames() ! FindGame(player)
+        context.become(waitingRoom(myUid))
+      case _ =>
+    }
+    case x : Players =>
+      out ! Json.toJson(x)
+    case Invitation(otherPlayer) =>
+      out ! Json.toJson(Value("Invitation", otherPlayer))
+  }
+
+  def sendingInvitationWaitingRoom(myUid: String) = LoggingReceive {
+    case Messages.Room(room) =>
+      context.become(sendingInvitation(myUid, room))
+    case Company(company) =>
+      company ! Invitation(myUid)
+      context.become(waitingRoom(myUid))
+  }
+
+  def sendingInvitation(myUid: String, room: ActorRef) = LoggingReceive {
+    case Company(company) =>
+      company ! Invitation(myUid)
+      room ! PlayerConnected(myUid)
+      context.become(waitingForSnake(myUid))
+  }
+
+  def waitingRoom(myUid: String) = LoggingReceive {
+    case Messages.Room(room) =>
+      room ! PlayerConnected(myUid)
+      context.become(waitingForSnake(myUid))
+  }
+
+  def waitingForSnake(myUid: String) = LoggingReceive {
+    case x : SnakeCreated =>
+      val snake = x.snake
+      context.system.scheduler.schedule(1.second, 10.seconds, sender, GetState)
+      out ! Json.toJson(Value("Status", "Snake created"))
+      println("playing")
+      context.become(playing(snake))
+  }
+
+  def playing(snake: ActorRef) = LoggingReceive {
+    case x : JsValue => (x \ "t").as[String] match {
       case "Direction" =>
         val dir = (x \ "dir").as[String]
         dir match {
@@ -38,31 +97,12 @@ class WebSocketActor(out: ActorRef) extends Actor with ActorLogging {
           case "Right" => snake ! Right
           case _ =>
         }
-      case "FindPlayers" =>
-        Lobby() ! GetList
-      case "Connect" =>
-        val player = (x \ "player").as[String]
-        AllGames() ! FindGame(player)
       case _ =>
     }
-    case Messages.Room(room) =>
-      room ! PlayerConnected(myUid)
-    case x : SnakeCreated =>
-      snake = x.snake
-      context.system.scheduler.schedule(0.second, 100.millis, sender, GetState)
-      out ! Json.toJson(Value("Status", "Snake created"))
-    case x : Players =>
-      myUid = x.me
-      out ! Json.toJson(x)
-    case Company(company) =>
-      company ! Invitation(myUid)
-    case Invitation(otherPlayer) =>
-      out ! Json.toJson(Value("Invitation", otherPlayer))
     case State(snake1, snake2, food) =>
       val coorSnake1 = snake1.map(pair => new Coordinate(pair._1, pair._2))
       val coorSnake2 = snake2.map(pair => new Coordinate(pair._1, pair._2))
       out ! Json.toJson(StateCoordinates(coorSnake1, coorSnake2, new Coordinate(food._1, food._2)))
-    case _ =>
   }
 }
 
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,19 +7,8 @@ import play.api.mvc._
 import play.api.Play.current
 
 class Application extends Controller {
-  AllGames().tell(Messages.Pair("player1", "player2"), AllGames())
-
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
-  }
-
-  def generatePair() : String = {
-    //    val player1 = UUID.randomUUID.toString
-    //    val player2 = UUID.randomUUID.toString
-    val player1 = "59b54598-ef38-4995-b659-fbea3ecbe317"
-    val player2 = "23523a7b-af9e-4fab-8703-bcbdb03e0071"
-    AllGames() ! Messages.Pair(player1, player2)
-    "Player1: " + player1 + "\nPlayer2: " + player2
   }
 
   def ws = WebSocket.acceptWithActor[JsValue, JsValue] { request => out =>
